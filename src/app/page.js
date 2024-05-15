d0d1397e1249d0a31973f6da71dc426f1aec26a3
Yago <yagomateoslerma@gmail.com>
2024-05-15 16:20:44 +0530

    initial

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import PublicNavbar from "../components/public/navbar/page"
+
+export default function Home() {
+  return (
+    <>
+    <PublicNavbar/>
+    <div>Hompepage content goes here</div>
+    </>
+  );
+}
