3cf3fd968a1ae903d0ccb85ae1a4e583593da10a
Yago <yagomateoslerma@gmail.com>
2024-06-18 11:58:46 +0530

    fixing build errors

--- src/app/search/page.js
+++ src/app/search/page.js
@@ -1,14 +1,13 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import PublicPageContainer from '../../components/containers/publicPageContainer';
 import { searchProducts } from '../../lib/actions/search/searchProducts';
 import { useRouter, useSearchParams } from 'next/navigation';
 import SingleFeaturedProduct from '../../components/public/sections/snippets/singleFeaturedProduct';
 
-export default function SearchResult() {
-
-    const [result, setResult] = useState(null)
+function SearchResultComponent() {
+    const [result, setResult] = useState(null);
 
     const router = useRouter();
     const searchParams = useSearchParams();
@@ -20,15 +19,14 @@ export default function SearchResult() {
             console.log(searchQuery);
 
             const getSearchResults = async () => {
-
                 try {
                     const searchResult = await searchProducts(searchQuery);
-                    console.log(searchResult)
-                    searchResult && searchResult != '' ? setResult(searchResult) : setResult(null)
+                    console.log(searchResult);
+                    searchResult && searchResult != '' ? setResult(searchResult) : setResult(null);
                 } catch (error) {
-                    console.log(error)
+                    console.log(error);
                 }
-            }
+            };
 
             getSearchResults();
 
@@ -40,13 +38,19 @@ export default function SearchResult() {
         <PublicPageContainer>
             <div className='container mx-auto my-8 px-4 max-w-7xl sm:px-6 lg:px-8'>
                 <div className='flex items-center gap-5 w-full'>
-                    {result && result.map((rst, key) => {
-
-                        return <SingleFeaturedProduct key={key} featuredProduct={rst} />
-
-                    })}
+                    {result && result.map((rst, key) => (
+                        <SingleFeaturedProduct key={key} featuredProduct={rst} />
+                    ))}
                 </div>
             </div>
         </PublicPageContainer>
+    );
+}
+
+export default function SearchResult() {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <SearchResultComponent />
+        </Suspense>
     );
 }
