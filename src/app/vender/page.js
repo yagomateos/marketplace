dee9874d037b90a883d2cb1b299240828f1839da
Yago <yagomateoslerma@gmail.com>
2025-02-05 16:43:10 +0530

    rewoke

--- src/app/vender/page.js
+++ src/app/vender/page.js
@@ -14,47 +14,47 @@ import { useRouter } from 'next/navigation'
 
 export default function Vender() {
 
-  return <div>comes here</div> 
 
-//     const [step, setStep] = useState(0)
-//     const { data: session , status } = useSession()
-//     const [reason, setReason] = useState(null)
-//     const [options, setOptions] = useState([])
 
-//     const router = useRouter()
+    const [step, setStep] = useState(0)
+    const { data: session , status } = useSession()
+    const [reason, setReason] = useState(null)
+    const [options, setOptions] = useState([])
 
-//  useEffect(() => {
-//     if (status === "loading") {
-//       // Session is still being fetched, no action needed yet
-//       return;
-//     }
+    const router = useRouter()
 
-//     if (status === "unauthenticated") {
-//       // If the user is not logged in, redirect them to the login page or home
-//       router.push("/");
-//     }
-//   }, [status, router]);
+ useEffect(() => {
+    if (status === "loading") {
+      // Session is still being fetched, no action needed yet
+      return;
+    }
 
-//     const scrollToTop = () => {
-//         window.scrollTo({
-//           top: 0,
-//           behavior: 'smooth'
-//         });
-//       };
+    if (status === "unauthenticated") {
+      // If the user is not logged in, redirect them to the login page or home
+      router.push("/");
+    }
+  }, [status, router]);
 
-//       useEffect(() => {
-//         scrollToTop()
-//       }, [step])
+    const scrollToTop = () => {
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth'
+        });
+      };
+
+      useEffect(() => {
+        scrollToTop()
+      }, [step])
       
 
-//     return (
-//         <PublicPageContainer>
-//             {step === 0 && (<FirstPage setStep={setStep} />)}
-//             {step === 1 && (<SecondPage setStep={setStep} user={session.user} />)}
-//             {step === 2 && (<ThirdPage setStep={setStep} reason={reason} setReason={setReason} scrollToTop={scrollToTop}/>)}
-//             {step === 3 && (<FourthPage setStep={setStep} options={options} setOptions={setOptions} />)}
-//             {step === 4 && (<FifthPage setStep={setStep} />)}
-//             {step === 5 && (<Storesetup reason={reason} options={options} />)}
-//         </PublicPageContainer>
-//     )
+    return (
+        <PublicPageContainer>
+            {step === 0 && (<FirstPage setStep={setStep} />)}
+            {step === 1 && (<SecondPage setStep={setStep} user={session.user} />)}
+            {step === 2 && (<ThirdPage setStep={setStep} reason={reason} setReason={setReason} scrollToTop={scrollToTop}/>)}
+            {step === 3 && (<FourthPage setStep={setStep} options={options} setOptions={setOptions} />)}
+            {step === 4 && (<FifthPage setStep={setStep} />)}
+            {step === 5 && (<Storesetup reason={reason} options={options} />)}
+        </PublicPageContainer>
+    )
 }
